89bd5f0606d1e41c34fcb0c8a98ee1d15e1e1517
Agustin <agustin.lopezfredes@gmail.com>
2019-11-28 00:17:41 -0300

    Added rotation for data augmentation

--- model.py
+++ model.py
@@ -12,22 +12,26 @@ full_data = pd.read_csv("train.csv")
 print(full_data.shape)
 
 #Print 10 random number
-
-for i,_  in enumerate(np.random.randint(42000, size = 9)):
+fig, ax = plt.subplots(nrows = 5, ncols = 2, figsize = (15, 5))
+for i, pic_number  in enumerate(np.random.randint(42000, size = 10)):
     #Convert data into array and strip the first value corresponding to the label
-    label, image_array = np.split(np.asarray(full_data.loc[i]), [1]) #Check if loc shouldn't be used to select rows by integer
+    label, image_array = np.split(np.asarray(full_data.loc[pic_number]), [1]) #Check if loc shouldn't be used to select rows by integer
     image_array = image_array.reshape((28, 28)) #Convert to image format
 
-    plt.imshow(image_array, cmap = 'gray')
-    plt.title('Label: ' + str(label[0]))
-    plt.show()
+    ax[i // 2, i % 2].imshow(image_array, cmap = 'gray')
+    ax[i // 2, i % 2].set_title('Label: ' + str(label[0]))
+fig.subplots_adjust(wspace = 1, hspace = 1)
+plt.show()
 
-labels = np.asarray(full_data['label'])
-images = np.asarray(full_data.loc[:, full_data.columns != 'label'])
+labels_original = np.asarray(full_data['label'])
+images_original = np.asarray(full_data.loc[:, full_data.columns != 'label'])
+images_original = images_original.reshape(images_original.shape[0], 28, 28, 1)
 
-images = images.reshape(images.shape[0], 28, 28, 1)
+#Augment data by rotating images
+labels_augmented = np.concatenate((labels_original, labels_original)) #The labels remain the same
+images_augmented = np.concatenate((images_original, images_original.transpose(0, 2, 1, 3))) #Transpose two of the axis in order to rotate the image
 
-X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size = 0.05) 
+X_train, X_test, y_train, y_test = train_test_split(images_augmented, labels_augmented, test_size = 0.05) 
 
 #Convert labels to categorical values 
 #to_categorical is a one-hot conversion. The data must be converted to a numpy array beforehand in order to work well
